c8cd9fb69b3c57a3d6237b95a55c40d7f9a5d86a
chiha choi <chihachoi@outlook.com>
2021-08-30 19:14:37 +0100

    fix button url and readme

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ import topImageHd from "./images/top_image_scale_2x.png";
 import backgroundImage from "./images/background_scale_1x.jpeg";
 import backgroundImageHd from "./images/background_scale_2x.jpeg";
 
-let initialTime, cashValue, ButtonUrl;
+let initialTime, cashValue, buttonUrl;
 
 if (typeof process.env.REACT_APP_INITIAL_TIME === "undefined") {
   console.log("failed to load process.env.REACT_APP_ variables ");
 } else {
   initialTime = process.env.REACT_APP_INITIAL_TIME;
   cashValue = process.env.REACT_APP_CASH_VALUE;
-  ButtonUrl = process.env.REACT_APP_BUTTON_URL;
+  buttonUrl = process.env.REACT_APP_BUTTON_URL;
 }
 //load timer from previous session
 if (typeof window.localStorage.currentTimer == 'string'){
@@ -49,7 +49,7 @@ const App = () => {
           Get your <em className={"neonLight"}>free £{cashValue}</em> now
         </p>
         <Timer time={time} />
-        <a href="https://www.jackpotjoy.com/">
+        <a href={buttonUrl}>
           <button>
             <p className="button-text">Opt in</p>
           </button>
